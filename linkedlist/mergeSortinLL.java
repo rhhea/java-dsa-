3cfe178a65f12601432836c097dfabaf312de92c
rhhea <rhheayadav@gmail.com>
2026-04-04 23:47:46 +0530

    merge sort on ll

--- linkedlist/mergeSortinLL.java
+++ linkedlist/mergeSortinLL.java
@@ -1,6 +1,36 @@
 package linkedlist;
-
+import java.util.LinkedList;
 public class mergeSortinLL {
+    public class Node{
+        int data;
+        Node next;
+
+        //constructors
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    //constant reference variables
+    public static Node head;
+    public static Node tail;
+
+
+    //mergesort method
+    public Node mergeSort(Node head){
+
+        //find mid
+        Node mid = getmid();
+
+        //divide
+        Node righth = mid.next;
+        mid.next = null;
+        Node newl = mergeSort(head); //left half
+        Node newr = mergeSort(righth);
+
+        //merge and return
+        return merge(newl,newr);
+    }
     //main method
     public static void main(String[] args) {
         
